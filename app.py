fc7e1f4ec43eff9d5f173f13a7b5b502b937d334
TrineOsterman <trine.ostermanbrevig@gmail.com>
2024-09-09 12:14:51 +0200

    fixade stavfel

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 def home():
     return render_template('login.html')
 
-@app.rout('/register')
+@app.route('/register')
 def register():
     return render_template('register.html')
 
