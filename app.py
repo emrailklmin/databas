92d75eafb384631051357fcdd9f843a5a9c3eaa2
TrineOsterman <trine.ostermanbrevig@gmail.com>
2024-09-09 12:11:06 +0200

    Lägger till väg för register

--- app.py
+++ app.py
@@ -4,7 +4,11 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return render_template('base.html')
+    return render_template('login.html')
+
+@app.rout('/register')
+def register():
+    return render_template('register.html')
 
 if __name__ == '__main__':
     # Run the app on port 5000
