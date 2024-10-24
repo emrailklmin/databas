1a94eb8fe529b2cac3736647bd2486cd78399aa9
emrailklmin <emrailklmin@gmail.com>
2024-10-24 10:06:31 +0200

    cleaned

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template, request, redirect, url_for, session, flash, get_flashed_messages
 import pyodbc
 import pickle
-from credentials_public import SQL_SERVER  # Import hidden credentials
+from credentials import SQL_SERVER  # Import hidden credentials
 
 app = Flask(__name__)
 app.secret_key = 'your_secret_key'  # Required for flash messages
@@ -282,34 +282,6 @@ def delete_entry():
 
     # Redirect back to the finance page
     return redirect(url_for('finance'))
-
-
-# @app.route('/finance', methods=['GET', 'POST'])
-# def finance():
-#     global incomes, expenses, savings
-    
-#     # Handle form submission
-#     if request.method == 'POST':
-#         type_of_entry = request.form['Type']
-#         description = request.form['Description']
-#         amount = float(request.form['Amount'])
-
-#         # Add to the appropriate dictionary
-#         if type_of_entry == 'income':
-#             incomes[description] = amount
-#         elif type_of_entry == 'expense':
-#             expenses[description] = amount
-#         elif type_of_entry == 'savings':
-#             savings[description] = amount
-
-#     # Calculate total income and expenses
-#     total_income = sum(incomes.values())
-#     total_expenses = sum(expenses.values())
-#     total_savings = sum(savings.values())
-#     net_result = total_income - total_expenses
-#     net_result_after_savings = net_result - total_savings
-
-#     return render_template('finance.html', incomes=incomes, expenses=expenses, savings=savings, total_income=total_income, total_expenses=total_expenses, total_savings=total_savings, net_result=net_result, net_result_after_savings=net_result_after_savings)
 
 if __name__ == '__main__':
     # Run the app on port 8000
